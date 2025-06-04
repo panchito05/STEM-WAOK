a8907cac7185703916eb9882ab61588fdaf34cf9
WAOK <81894454+panchito05@users.noreply.github.com>
2025-06-04 17:48:42 -0400

    Add Jest and PostCSS configuration files

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/client/src/$1',
+    '^@shared/(.*)$': '<rootDir>/shared/$1',
+  },
+  testMatch: [
+    '**/__tests__/**/*.ts?(x)',
+    '**/?(*.)+(spec|test).ts?(x)'
+  ],
+  transform: {
+    '^.+\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.json',
+    }],
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
